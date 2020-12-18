9d59be30f83db2b1402b60603e07c9450ba4f742
LeonardoVAC <37460201+leonardovac@users.noreply.github.com>
2020-12-18 11:29:29 -0300

    Add files via upload

---
+++ ARAM Skin Boost v2/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿using System.Windows.Forms;
+
+namespace ARAM_Skin_Boost_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label5;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new System.Drawing.Font("Consolas", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            label5.ForeColor = System.Drawing.Color.White;
+            label5.Location = new System.Drawing.Point(77, 5);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(152, 17);
+            label5.TabIndex = 13;
+            label5.Text = "VAC\'s Skin Booster";
+            label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(187)))), ((int)(((byte)(197)))));
+            this.label3.Location = new System.Drawing.Point(3, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 14);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "┍━ You can check the source at GitHub";
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Open Sans Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(187)))), ((int)(((byte)(197)))));
+            this.label2.Location = new System.Drawing.Point(48, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Written in C# and redone by";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(187)))), ((int)(((byte)(197)))));
+            this.label4.Location = new System.Drawing.Point(3, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 14);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "┕► Enjoy!";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(34)))), ((int)(((byte)(40)))));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 345);
+            this.panel1.TabIndex = 10;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::ARAM_Skin_Boost_v2.Properties.Resources.GitHub_Logo;
+            this.pictureBox2.Location = new System.Drawing.Point(217, 287);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Open Sans Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(187)))), ((int)(((byte)(197)))));
+            this.label7.Location = new System.Drawing.Point(225, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 18);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "VAC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(187)))), ((int)(((byte)(197)))));
+            this.label6.Location = new System.Drawing.Point(164, 331);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "This is a free release.";
+            this.label6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(43)))), ((int)(((byte)(50)))));
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(label5);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(310, 26);
+            this.panel2.TabIndex = 12;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(92)))), ((int)(((byte)(103)))));
+            this.label9.Location = new System.Drawing.Point(253, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 26);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "-";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            this.label9.MouseLeave += new System.EventHandler(this.label9_MouseLeave);
+            this.label9.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label9_MouseMove);
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(92)))), ((int)(((byte)(103)))));
+            this.label8.Location = new System.Drawing.Point(272, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 26);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "?";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            this.label8.MouseLeave += new System.EventHandler(this.label8_MouseLeave);
+            this.label8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label8_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(43)))), ((int)(((byte)(50)))));
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(92)))), ((int)(((byte)(103)))));
+            this.label1.Location = new System.Drawing.Point(291, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 26);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "X";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = global::ARAM_Skin_Boost_v2.Properties.Resources.btn1;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 78);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(310, 345);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "VAC\'s Skin Booster";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        /*private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Specify that the link was visited.
+            this.linkLabel1.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start("https://www.mpgh.net/");
+        }*/
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private Label label6;
+        private Label label7;
+        private PictureBox pictureBox2;
+        private Label label8;
+        private Label label9;
+    }
+}
+
---
+++ ARAM Skin Boost v2/Form1.cs
@@ -0,0 +1,217 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Management;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ARAM_Skin_Boost_v2
+{
+    public partial class Form1 : Form
+	{
+		//Start of form
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		// Bordeless movable form
+		public const int WM_NCLBUTTONDOWN = 0xA1;
+		public const int HT_CAPTION = 0x2;
+		[DllImportAttribute("user32.dll")]
+		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+		[DllImportAttribute("user32.dll")]
+		public static extern bool ReleaseCapture();
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			ReleaseCapture();
+			SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+		}
+
+		//GitHubs's button hotlink
+		private void pictureBox2_MouseMove(object sender, EventArgs e)
+		{
+			pictureBox2.Image = Properties.Resources.GitHub_Logo2;
+		}
+
+		private void pictureBox2_MouseLeave(object sender, EventArgs e)
+		{
+			pictureBox2.Image = Properties.Resources.GitHub_Logo;
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+           System.Diagnostics.Process.Start("https://github.com/leonardovac/ARAM-Skin-Booster");
+        }
+
+		// Close, help and minimize buttons
+		private void label1_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void label1_MouseMove(object sender, MouseEventArgs e)
+		{
+			label1.ForeColor = System.Drawing.ColorTranslator.FromHtml("#b73434");
+		}
+
+		private void label1_MouseLeave(object sender, EventArgs e)
+		{
+            label1.ForeColor = System.Drawing.ColorTranslator.FromHtml("#4a5c67");
+		}
+		private void label8_Click(object sender, EventArgs e)
+		{
+			string message = "- Original code by matix2 (UC);\n\nCheck his GitHub.";
+			string caption = "Kudos to matix2";
+			MessageBoxButtons buttons = MessageBoxButtons.OK;
+			DialogResult result;
+
+			result = MessageBox.Show(message, caption, buttons);
+			if (result == System.Windows.Forms.DialogResult.OK)
+			{
+				System.Diagnostics.Process.Start("https://github.com/matix2/aram-skin-booster");
+			}
+		}
+
+		private void label8_MouseMove(object sender, MouseEventArgs e)
+		{
+			label8.ForeColor = System.Drawing.ColorTranslator.FromHtml("#009486");
+		}
+
+		private void label8_MouseLeave(object sender, EventArgs e)
+		{
+			label8.ForeColor = System.Drawing.ColorTranslator.FromHtml("#4a5c67");
+		}
+
+		private void label9_Click(object sender, EventArgs e)
+		{
+			WindowState = FormWindowState.Minimized;
+		}
+
+		private void label9_MouseMove(object sender, MouseEventArgs e)
+		{
+			label9.ForeColor = System.Drawing.ColorTranslator.FromHtml("#009486");
+		}
+
+		private void label9_MouseLeave(object sender, EventArgs e)
+		{
+			label9.ForeColor = System.Drawing.ColorTranslator.FromHtml("#4a5c67");
+		}
+
+		// Button color change
+		private void pictureBox1_MouseLeave(object sender, EventArgs e)
+		{
+			pictureBox1.Image = Properties.Resources.btn1;
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			pictureBox1.Image = Properties.Resources.btn2;
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			pictureBox1.Image = Properties.Resources.btn3;
+		}
+
+		//End of form
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				ValueTuple<string, string> valueTuple = GetInfo();
+				PostRequest(valueTuple.Item1, valueTuple.Item2);
+				Thread.Sleep(250);
+			}
+			catch (Exception)
+			{
+				string message = "Something went wrong...";
+				string caption = "Error";
+				MessageBoxButtons buttons = MessageBoxButtons.OK;
+				DialogResult result;
+
+				result = MessageBox.Show(message, caption, buttons);
+				if (result == System.Windows.Forms.DialogResult.OK)
+				{
+					Application.Exit();
+				}
+			}
+		}
+
+		static ValueTuple<string, string> GetInfo()
+		{
+			string text = "";
+			string text2 = "";
+			ManagementClass managementClass = new ManagementClass("Win32_Process");
+			foreach (ManagementBaseObject managementBaseObject in managementClass.GetInstances())
+			{
+				ManagementObject managementObject = (ManagementObject)managementBaseObject;
+				if (managementObject["Name"].Equals("LeagueClientUx.exe"))
+				{
+					foreach (object obj in Regex.Matches(managementObject["CommandLine"].ToString(), string_0, regexOptions_0))
+					{
+						Match match = (Match)obj;
+						if (!string.IsNullOrEmpty(match.Groups["port"].ToString()))
+						{
+							text2 = match.Groups["port"].ToString();
+						}
+						else if (!string.IsNullOrEmpty(match.Groups["token"].ToString()))
+						{
+							text = match.Groups["token"].ToString();
+						}
+					}
+				}
+			}
+			if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(text2))
+			{
+				string message = "Is your client running?";
+				string caption = "LeagueClientUx.exe not found!";
+				MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+				DialogResult result;
+
+				result = MessageBox.Show(message, caption, buttons);
+				if (result == System.Windows.Forms.DialogResult.Yes)
+				{
+                    Application.Exit();
+				}
+			}
+			return new ValueTuple<string, string>(text, text2);
+		}
+		
+		static void PostRequest(string string_1, string string_2)
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+			ServicePointManager.ServerCertificateValidationCallback = (RemoteCertificateValidationCallback)Delegate.Combine(ServicePointManager.ServerCertificateValidationCallback, new RemoteCertificateValidationCallback(Class1.NewClass.Main));
+			RestClient restClient = new RestClient("https://127.0.0.1:" + string_2)
+			{
+				Authenticator = new HttpBasicAuthenticator("riot", string_1)
+			};
+			RestRequest request = new RestRequest("/lol-champ-select/v1/team-boost/purchase", Method.POST); // This is everything you need to know, these 200 lines of code could be reduced to 20.
+			restClient.Execute(request);
+		}
+
+		static readonly string string_0 = "\"--remoting-auth-token=(?'token'.*?)\" | \"--app-port=(?'port'|.*?)\"";
+        static readonly RegexOptions regexOptions_0 = RegexOptions.Multiline;
+    }
+	public class Class1
+	{
+		internal bool Main(object object_0, X509Certificate x509Certificate_0, X509Chain x509Chain_0, SslPolicyErrors sslPolicyErrors_0)
+		{
+			return true;
+		}
+
+        public static RemoteCertificateValidationCallback Callback { get; }
+        public static Class1 NewClass { get; } = new Class1();
+    }
+}
